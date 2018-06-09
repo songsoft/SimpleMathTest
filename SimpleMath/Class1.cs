58936ed104bbbcfb928aeb7ef8f00ddd070d9999
hongs <hongs@DESKTOP-QR5OTRN>
2018-06-09 00:23:23 -0500

    test

---
+++ SimpleMath/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleMath
+{
+    public static class MathFunctions
+    {
+        public static int Fibonacci(int factor)
+        {
+            if (factor < 2)
+                return factor;
+            int x = Fibonacci(--factor);
+            int y = Fibonacci(--factor);
+            return x + y;
+        }
+
+        /******************************************************************************************/
+    }
+}
